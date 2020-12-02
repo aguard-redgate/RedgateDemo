d8666a40e08f1ed08239455d6e7991d6005fc1e4
Andrew Guard <andrew.guard@red-gate.com>
2020-12-02 14:45:22 -0600

    alter tables

---
+++ Stored Procedures/dbo.SelectAllContacts.sql
@@ -0,0 +1,19 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[SelectAllContacts]
+-- Add the parameters for the stored procedure here	
+AS
+BEGIN
+    SELECT Address1,
+           Address2,
+           Address3
+    FROM dbo.Contacts;
+END;
+GO
--- Tables/dbo.Contacts.sql
+++ Tables/dbo.Contacts.sql
@@ -8,7 +8,8 @@ CREATE TABLE [dbo].[Contacts]
 [Address2] [nvarchar] (128) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [Address3] [nvarchar] (128) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [JoiningDate] [datetime] NULL CONSTRAINT [DF__Contacts__Joinin__2A4B4B5E] DEFAULT (getdate()),
-[Email] [nvarchar] (256) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[Email] [nvarchar] (256) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[SocialSecurity] [int] NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Contacts] ADD CONSTRAINT [PK_Contacts] PRIMARY KEY CLUSTERED  ([ID]) ON [PRIMARY]
